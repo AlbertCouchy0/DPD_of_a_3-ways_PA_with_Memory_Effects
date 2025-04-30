2ac6a8270b9b9e8d72b75002d03421003fbd3625
AlbertCouchy0 <albert.couchy.04@gmail.com>
2025-04-30 23:20:47 +0800

    完成了MATLAB仿真环境

---
+++ Simulation/ReadFile.m
@@ -0,0 +1,38 @@
+function [Data_matrix,dim]=ReadFile()
+% Data_matrix每一列对应的是
+% Data_matrix=[Sys_x_i, Sys_x_q,...
+%              PA1_y_i, PA1_y_q,...
+%              PA2_y_i, PA2_y_q,...
+%              PA3_y_i, PA3_y_q,...
+%              Sys_y_i, Sys_y_q];
+%% 读文件
+fileID = fopen('Data\Sys_input_i.txt'); C = textscan(fileID, '%f'); Sys_input_i = C{1, 1}; fclose(fileID);
+fileID = fopen('Data\Sys_input_q.txt'); C = textscan(fileID, '%f'); Sys_input_q = C{1, 1}; fclose(fileID);
+
+fileID = fopen('Data\PA1_output_i.txt'); C = textscan(fileID, '%f'); PA1_output_i = C{1, 1}; fclose(fileID);
+fileID = fopen('Data\PA1_output_q.txt'); C = textscan(fileID, '%f'); PA1_output_q = C{1, 1}; fclose(fileID);
+fileID = fopen('Data\PA2_output_i.txt'); C = textscan(fileID, '%f'); PA2_output_i = C{1, 1}; fclose(fileID);
+fileID = fopen('Data\PA2_output_q.txt'); C = textscan(fileID, '%f'); PA2_output_q = C{1, 1}; fclose(fileID);
+fileID = fopen('Data\PA3_output_i.txt'); C = textscan(fileID, '%f'); PA3_output_i = C{1, 1}; fclose(fileID);
+fileID = fopen('Data\PA3_output_q.txt'); C = textscan(fileID, '%f'); PA3_output_q = C{1, 1}; fclose(fileID);
+
+fileID = fopen('Data\Sys_output_i.txt');C = textscan(fileID, '%f'); Sys_output_i = C{1, 1}; fclose(fileID);
+fileID = fopen('Data\Sys_output_q.txt');C = textscan(fileID, '%f'); Sys_output_q = C{1, 1}; fclose(fileID);
+%% 数据处理
+dim = length(Sys_input_i) / 2;
+% 这么写的原因是因为其奇数位的数据为时间
+[Sys_x_i, Sys_x_q,PA1_y_i, PA1_y_q,PA2_y_i, PA2_y_q,PA3_y_i, PA3_y_q,Sys_y_i, Sys_y_q] = deal(zeros(dim, 1));
+for k = 1:dim
+    [Sys_x_i(k), Sys_x_q(k), Sys_y_i(k), Sys_y_q(k)] = deal(Sys_input_i(2 * k), Sys_input_q(2 * k), Sys_output_i(2 * k), Sys_output_q(2 * k));
+    [PA1_y_i(k), PA1_y_q(k)]=deal(PA1_output_i(2 * k),PA1_output_q(2 * k));
+    [PA2_y_i(k), PA2_y_q(k)]=deal(PA2_output_i(2 * k),PA2_output_q(2 * k));
+    [PA3_y_i(k), PA3_y_q(k)]=deal(PA3_output_i(2 * k),PA3_output_q(2 * k));
+    
+end 
+
+Data_matrix=[Sys_x_i, Sys_x_q,...
+             PA1_y_i, PA1_y_q,...
+             PA2_y_i, PA2_y_q,...
+             PA3_y_i, PA3_y_q,...
+             Sys_y_i, Sys_y_q];
+end
---
+++ Version1/Plot_AM.m
@@ -0,0 +1,16 @@
+function Plot_AM(X,Y,name,lg)
+y=abs(Y)./max(abs(Y));
+x=abs(X)./max(abs(X));
+
+figure()
+plot(x,y,'r.');
+hold on;
+plot(x,x,'b-','LineWidth',1.8);
+xlabel('Normalized Input Amplitude');
+ylabel('Normalized Output Amplitude');
+L=legend(lg);
+L.Location='northwest';
+title(name);
+grid on;grid minor;
+
+end
