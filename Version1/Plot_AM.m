105e2b6d7aae1f23108bfac24b540df5912eb94a
AlbertCouchy0 <albert.couchy.04@gmail.com>
2025-05-01 11:32:43 +0800

    测试了FNN

--- Version1/Plot_AM.m
+++ Version1/Plot_AM.m
@@ -5,7 +5,7 @@ x=abs(X)./max(abs(X));
 figure()
 plot(x,y,'r.');
 hold on;
-plot(x,x,'b-','LineWidth',1.8);
+plot(x,x,'g-','LineWidth',1.8);
 xlabel('Normalized Input Amplitude');
 ylabel('Normalized Output Amplitude');
 L=legend(lg);
